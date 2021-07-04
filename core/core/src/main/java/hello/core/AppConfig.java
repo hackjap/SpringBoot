b6b903f2bb0364d919773dd96457f8be7a4e8b81
hackjap <hackjap1015@gmail.com>
2021-07-04 10:45:27 +0900

    :art:스프링으로 전환

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -8,27 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 역할을 생성하고 구현체까지 부여하여 service에는 오직 실행에만 집중할 수 있도록 한다. = DI(Dependancy Injection) 의존관계 주입
+@Configuration
 public class AppConfig { // AppConfig = 공연 기획자 ( 역할과 구현을 분리 )
 
-    public MemberService memberService(){
+    @Bean
+    public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() { // 역할과 구현을 한 눈에 파악됨
+    @Bean
+    public MemoryMemberRepository memberRepository() { // 역할과 구현을 한 눈에 파악됨
         return new MemoryMemberRepository();
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(memberRepository(),discountPolicy()) ;
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    public DiscountPolicy discountPolicy(){
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //      return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
-
 
 
 }
--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -4,19 +4,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     // psvm : public static void main 단축키
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();    // app config에서 memberservice를 결정
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();    // app config에서 memberservice를 결정
 //        MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);    // 스프링 컨테이너 생성
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // name:빈 객체 메소드 명, 타입
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("new Member = " + member);
+        System.out.println("new Member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
 
     }
